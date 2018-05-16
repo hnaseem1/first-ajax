e01719f74634278694cf4121b1bfb9ae515287f4
Hasan Naseem <hn92@live.com>
2018-05-15 21:22:39 -0400

    step3

--- ajax.js
+++ ajax.js
@@ -1,14 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
 
- $.ajax({
+var button = document.querySelector('button')
+button.addEventListener('click', function() {
 
-    url: 'http://first-ajax-api.herokuapp.com/count',
+  console.log('Initializing...');
+  $.ajax({
+
+    url: 'http://first-ajax-api.herokuapp.com/ping',
     method: 'GET',
-    dataType: 'html'
+    dataType: 'text'
+  }).done(function(responseData) {
+    console.log(responseData);
+    var section = document.createElement('section');
+    var node = document.createTextNode(responseData);
+    section.appendChild(node);
+    var element = document.querySelector('body')
+    element.appendChild(section)
+  })
+});
+
+ var response = $.ajax({
+
+    url: 'http://first-ajax-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'text'
 
   });
 
+  response.done(function(responseData) {
 
 
+
+
+  })
 
 });
