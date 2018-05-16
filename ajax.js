1d119282799089b39090391b7ac21cf21211cd92
Hasan Naseem <hn92@live.com>
2018-05-16 15:39:41 -0400

    step 7 done

--- ajax.js
+++ ajax.js
@@ -1,36 +1,56 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-var button = document.querySelector('button')
-button.addEventListener('click', function() {
+  var button = document.querySelector('button')
+  button.addEventListener('click', function() {
 
-  console.log('Initializing...');
+  // console.log('Initializing...');
+
   $.ajax({
 
     url: 'http://first-ajax-api.herokuapp.com/ping',
     method: 'GET',
     dataType: 'text'
+
   }).done(function(responseData) {
-    console.log(responseData);
+
+    // console.log(responseData);
+
     var section = document.createElement('section');
     var node = document.createTextNode(responseData);
+    var element = document.querySelector('body');
+
     section.appendChild(node);
-    var element = document.querySelector('body')
-    element.appendChild(section)
+    element.appendChild(section);
+
+    console.log('Success!!');
+
+    }).fail(function() {
+
+     console.log('oops something went wrong');
+
+    }).always(function() {
+
+     console.log('Request ended!');
+
+    })
+
   })
-});
 
- var response = $.ajax({
+  // Step 7
 
-    url: 'http://first-ajax-api.herokuapp.com/',
+  var button2 = document.querySelector('#button2')
+  button2.addEventListener('click', function() {
+  $.ajax({
+
+    url: 'http://first-ajax-api.herokuapp.com/count',
     method: 'GET',
     dataType: 'text'
 
-  });
+  }).done(function(data) {
 
-  response.done(function(responseData) {
+    console.log(data);
 
-
-
+    })
 
   })
 
